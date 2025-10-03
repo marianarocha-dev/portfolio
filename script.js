9e4ba2fbad99745480010bc4e678e643e6a1b14f
Mari Rocha <rochamari.amaral@gmail.com>
2025-10-02 23:50:10 -0300

    novo portfolio

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const menuIcon = document.querySelector('#menu-icon');
+const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+const navLinksItems = document.querySelectorAll('.nav-links li');
 
-menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
+
+hamburger.addEventListener('click', () => {
+
+    navLinks.classList.toggle('nav-active');
+
+  
+    hamburger.classList.toggle('toggle');
+});
+
+
+navLinksItems.forEach(link => {
+    link.addEventListener('click', () => {
+        
+        if (navLinks.classList.contains('nav-active')) {
+            navLinks.classList.remove('nav-active');
+            hamburger.classList.remove('toggle');
+        }
+    });
+});
